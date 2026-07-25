07e8ec0cf6262cec3357f847b11a9e8916b37e16
LamTienKiet <lamtienkiet123@gmail.com>
2026-07-25 23:57:15 +0700

    product.service.ts

---
+++ frontend/src/services/product.service.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import type {
+  CreateProductRequest,
+  ProductResponse,
+  UpdateProductRequest,
+} from "../type_auth_api/products/product.api";
+
+const API_URL = "http://localhost:3000/product";
+
+export async function getProducts(): Promise<ProductResponse[]> {
+  const res = await axios.get<ProductResponse[]>(API_URL);
+  return res.data;
+}
+
+export async function getProductById(id: number): Promise<ProductResponse> {
+  const res = await axios.get<ProductResponse>(`${API_URL}/${id}`);
+  return res.data;
+}
+
+export async function createProducts(
+  data: CreateProductRequest,
+): Promise<ProductResponse> {
+  const res = await axios.post<ProductResponse>(API_URL, data);
+  return res.data;
+}
+
+export async function updateProduct(
+  id: number,
+  data: UpdateProductRequest,
+): Promise<ProductResponse> {
+  const res = await axios.patch<ProductResponse>(`${API_URL}/${id}`, data);
+
+  return res.data;
+}
+
+export async function deleteProduct(id: number): Promise<void> {
+  await axios.delete(`${API_URL}/${id}`);
+}
