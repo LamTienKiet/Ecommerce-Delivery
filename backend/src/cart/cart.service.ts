ade24d6248af081ee8b95739ed717342df44d9bb
LamTienKiet <lamtienkiet123@gmail.com>
2026-08-06 00:46:26 +0700

    addToCart Function

--- backend/src/cart/cart.service.ts
+++ backend/src/cart/cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCartDto } from './dto/create-cart.dto';
 
@@ -6,33 +6,48 @@ import { CreateCartDto } from './dto/create-cart.dto';
 export class CartService {
   constructor(private prismaService: PrismaService) {}
 
-  createCart(createCartDto: CreateCartDto) {
-    return this.prismaService.cart.create({
-      data: {
-        userId: createCartDto.userId,
-      },
-      include: {
-        cartItems: {
-          include: {
-            product: true,
+  async addToCart(createCartDto: CreateCartDto) {
+    const { userId, productId, quantity } = createCartDto;
+
+    //transaction
+    return this.prismaService.$transaction(async (tx) => {
+      const product = await tx.product.findUnique({
+        where: {
+          id: productId,
+        },
+      });
+
+      if (!product) {
+        throw new BadRequestException(
+          'Product with id ${productId} is not found',
+        );
+      }
+
+      const cart = await tx.cart.upsert({
+        where: { userId },
+        update: {},
+        create: { userId },
+      });
+
+      // 3. Thêm mới hoặc cộng dồn số lượng sản phẩm trong giỏ
+      await tx.cartItem.upsert({
+        where: {
+          cartId_productId: {
+            cartId: cart.id,
+            productId: productId,
           },
         },
-      },
-    });
-  }
-  // getCartByUserId()
-  // getCartById()
-  // clearCart()
-  // deleteCart()
-
-  findUserCart(userId: number) {
-    return this.prismaService.cart.findUnique({
-      where: {
-        userId: userId,
-      },
-      include: {
-        user: true,
-      },
+        update: {
+          quantity: {
+            increment: quantity,
+          },
+        },
+        create: {
+          cartId: cart.id,
+          productId: productId,
+          quantity: quantity,
+        },
+      });
     });
   }
 }
