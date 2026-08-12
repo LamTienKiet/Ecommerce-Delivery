79a062fef18a22bf9218517a187a5febfa892c60
LamTienKiet <lamtienkiet123@gmail.com>
2026-08-12 23:39:12 +0700

    Create Profile Structure

--- backend/src/profile/dto/create-profile.dto.ts
+++ backend/src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,32 @@
-export class CreateProfileDto {}
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateProfileDto {
+  @IsNotEmpty()
+  @IsString()
+  fullName: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthday?: string;
+
+  @IsOptional()
+  @IsString()
+  gender?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+}
--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ChangePasswordDto } from './dto/changePassword.dto';
 
 @Injectable()
 export class ProfileService {
-  create(createProfileDto: CreateProfileDto) {
-    return 'This action adds a new profile';
+  constructor(private prismaService: PrismaService) {}
+
+  getProfile(accountId: number) {
+    return this.prismaService.account.findUnique({
+      where: {
+        id: accountId,
+      },
+      include: {
+        user: true,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all profile`;
+  updateProfile(accountId: number, updateProfileDto: UpdateProfileDto) {
+    return this.prismaService.user.update({
+      where: {
+        accountId,
+      },
+      data: updateProfileDto,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} profile`;
-  }
-
-  update(id: number, updateProfileDto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} profile`;
+  changePassword(accountId: number, changePasswordDto: ChangePasswordDto) {
+    return this.prismaService.account.update({
+      where: {},
+    });
   }
 }
